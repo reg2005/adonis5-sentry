f27530a9633860f0166b83807f4801f57e7ea741
Vladyslav Parashchenko <vladyslavparashchenko37@gmail.com>
2020-08-19 22:20:34 +0300

    style: remove debug messages

--- providers/SentryProvider.ts
+++ providers/SentryProvider.ts
@@ -13,7 +13,6 @@ export default class SentryProvider {
 
 	public boot(): void {
 		const config: typeof Config = this.container.use('Adonis/Core/Config')
-		console.log('confog', config.get('sentry'))
 		Sentry.init(config.get('sentry'))
 	}
 }
