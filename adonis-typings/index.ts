1f77101e9af2cee5bb3d1be16506cc594f56c146
Vladyslav Parashchenko <vladyslavparashchenko37@gmail.com>
2020-09-13 17:06:43 +0300

    fix(typing): fixed typo in adonis typings

--- adonis-typings/index.ts
+++ adonis-typings/index.ts
@@ -2,5 +2,5 @@ declare module '@ioc:Adonis/Addons/Sentry' {
 	import type { NodeOptions } from '@sentry/node'
 	export interface SentryConfig extends NodeOptions {}
 	import * as Sentry from '@sentry/node'
-	export default Senty
+	export default Sentry
 }
