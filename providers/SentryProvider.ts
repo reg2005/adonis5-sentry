e6fcc6e0c1f7db93687ffc244f430874ca81d5b1
Vladimir <vgololobov@stormwall.pro>
2021-05-06 13:23:50 +0300

    feat(): provider fix

--- providers/SentryProvider.ts
+++ providers/SentryProvider.ts
@@ -1,18 +1,18 @@
-import { IocContract } from '@adonisjs/fold/build'
+import { ApplicationContract } from '@ioc:Adonis/Core/Application'
 import Config from '@ioc:Adonis/Core/Config'
 import * as Sentry from '@sentry/node'
 
 export default class SentryProvider {
-	constructor(protected container: IocContract) {}
+	constructor(protected app: ApplicationContract) {}
 
 	public register(): void {
-		this.container.singleton('Adonis/Addons/Sentry', () => {
+		this.app.container.singleton('Adonis/Addons/Sentry', () => {
 			return { ...Sentry }
 		})
 	}
 
 	public boot(): void {
-		const config: typeof Config = this.container.use('Adonis/Core/Config')
+		const config: typeof Config = this.app.container.use('Adonis/Core/Config')
 		Sentry.init(config.get('sentry'))
 	}
 }
